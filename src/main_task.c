05df33711e92a4fc6f3c7d088cb5286661b301ac
zolkko <zolkko@gmail.com>
2014-05-20 22:19:07 +0400

    CRC-8 check has been added to Status Register Read

--- src/main_task.c
+++ src/main_task.c
@@ -18,34 +18,37 @@ void main_task(void * params)
 	sht1x_init();
 
 	uint8_t data;
-	uint8_t expect = SHT1X_SREG_LOW_RES_bm;
-	uint16_t temper = 0;
+	uint8_t expect; //SHT1X_SREG_LOW_RES_bm;
 	while (true) {
-		printf("Status Register Write\t");
-		if (sht1x_status_write(expect) == SHT1X_ERROR_OK) {
-			printf("OK\n");
-		} else {
-			printf("FAIL\n");
-		}
+		for (expect = 0; expect < 2; expect++) {
+			printf("============\nExpected value: %d", expect);
 
-		printf("Status Register Read\t");
-		if (sht1x_status_read(&data) == SHT1X_ERROR_OK) {
-			if (data == expect) {
-				printf("OK\t=\t%d\n", data);
+			printf("Status Register Write\t");
+			if (sht1x_status_write(expect) == SHT1X_ERROR_OK) {
+				printf("OK\n");
 			} else {
-				printf("INVALID=%d\texpect=%d\n", data, expect);
+				printf("FAIL\n");
 			}
-		} else {
-			printf("FAIL\n");
+
+			printf("Status Register Read\t");
+			if (sht1x_status_read(&data) == SHT1X_ERROR_OK) {
+				if (data == expect) {
+					printf("OK=%d\n", data);
+				} else {
+					printf("Got=%d Expect=%d\n", data, expect);
+				}
+			} else {
+				printf("FAIL\n");
+			}
+
+			vTaskDelay(500 / portTICK_PERIOD_MS);
 		}
 
-		printf("Read temperature");
+		/*printf("Read temperature");
 		if (sht1x_temperature_read(&temper) == SHT1X_ERROR_OK) {
 			printf("\tOK = %d\n", temper);
 		} else {
 			printf("\tFAIL\n");
-		}
-
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+		}*/
 	}
 }
--- src/sht1x.c
+++ src/sht1x.c
@@ -90,12 +90,17 @@
 #define sht1x_data_out_clear()	GPIOPinWrite(SHT1X_PORT_BASE, SHT1X_DATA_OUT_PIN, 0x00)
 #define sht1x_data_in_get()		GPIOPinRead(SHT1X_PORT_BASE, SHT1X_DATA_IN_PIN)
 
+#define SHT1X_DEFAULT_STATUS	(0x00)
+
+#define SHT1X_DEFAULT_CRC_INIT	(0x00)
+
 /**
  * State index
  */
 typedef enum {
 	SHT1X_SIDX_END = 0,
 
+	/* Read Temperature */
 	SHT1X_SIDX_RTEMPER_FIN,
 	SHT1X_SIDX_RTEMPER_ACK,
 	SHT1X_SIDX_RTEMPER_ACKC,
@@ -255,6 +260,9 @@ typedef struct {
 	uint16_t data;
 	uint8_t crc;
 
+	uint8_t status;
+	uint8_t crc_init;
+
 	uint8_t cmd[255];
 	uint8_t cmd_payload[24];
 } sht1x_device_t;
@@ -335,6 +343,8 @@ static uint8_t sreg_read_pattern[] = {
 	SHT1X_DATA_OUT_PIN, SHT1X_BOTH_PINS, /* c1 */
 	SHT1X_DATA_OUT_PIN, SHT1X_BOTH_PINS, /* c3 */
 };
+
+#define SREG_READ_SEQENCE	(0x07)
 
 /**
  * The bit pattern for "Status Register Read" command.
@@ -502,6 +512,34 @@ static sht1x_state_t states[] = {
 	/* SHT1X_SIDX_SREGW_PAYLOAD_ACKC */	{SHT1X_SOUT_GPIO,	SHT1X_BOTH_PINS,	0,	NULL,				0,	{SHT1X_SIDX_SREGW_PAYLOAD_ACKE, SHT1X_SIDX_SREGW_PAYLOAD_ACKE}, {SHT1X_ERROR_OK, SHT1X_ERROR_OK}},
 	/* SHT1X_SIDX_SREGW_PAYLOAD_ACKE */	{SHT1X_SOUT_GPIO,	SHT1X_DATA_OUT_PIN,	0,	NULL,				0,	{SHT1X_SIDX_END, SHT1X_SIDX_END}, {SHT1X_ERROR_OK, SHT1X_ERROR_OK}},
 };
+
+/**
+ * CRC-8 lookup table for polynomial x^8 + x^5 + x^4 + 1
+ * For more information see https://github.com/zolkko/blumenplace-uc/wiki/SHT1X-CRC-8-calculation
+ */
+const static uint8_t crc8_lut[] = {
+	0,   49,  98,  83,  196, 245, 166, 151, 185, 136, 219, 234, 125, 76,  31,  46,
+	67,  114, 33,  16,  135, 182, 229, 212, 250, 203, 152, 169, 62,  15,  92,  109,
+	134, 183, 228, 213, 66,  115, 32,  17,  63,  14,  93,  108, 251, 202, 153, 168,
+	197, 244, 167, 150, 1,   48,  99,  82,  124, 77,  30,  47,  184, 137, 218, 235,
+	61,  12,  95,  110, 249, 200, 155, 170, 132, 181, 230, 215, 64,  113, 34,  19,
+	126, 79,  28,  45,  186, 139, 216, 233, 199, 246, 165, 148, 3,   50,  97,  80,
+	187, 138, 217, 232, 127, 78,  29,  44,  2,   51,  96,  81,  198, 247, 164, 149,
+	248, 201, 154, 171, 60,  13,  94,  111, 65,  112, 35,  18,  133, 180, 231, 214,
+	122, 75,  24,  41,  190, 143, 220, 237, 195, 242, 161, 144, 7,   54,  101, 84,
+	57,  8,   91,  106, 253, 204, 159, 174, 128, 177, 226, 211, 68,  117, 38,  23,
+	252, 205, 158, 175, 56,  9,   90,  107, 69,  116, 39,  22,  129, 176, 227, 210,
+	191, 142, 221, 236, 123, 74,  25,  40,  6,   55,  100, 85,  194, 243, 160, 145,
+	71,  118, 37,  20,  131, 178, 225, 208, 254, 207, 156, 173, 58,  11,  88,  105,
+	4,   53,  102, 87,  192, 241, 162, 147, 189, 140, 223, 238, 121, 72,  27,  42,
+	193, 240, 163, 146, 5,   52,  103, 86,  120, 73,  26,  43,  188, 141, 222, 239,
+	130, 179, 224, 209, 70,  119, 36,  21,  59,  10,  89,  104, 255, 206, 157, 172
+};
+
+
+uint8_t sht1x_reverse_bits(uint8_t value);
+
+uint8_t sht1x_crc(uint8_t * data, size_t data_len, uint8_t initial_crc);
 
 
 static xSemaphoreHandle lock = NULL;
@@ -704,7 +742,7 @@ sht1x_error_t sht1x_status_write(uint8_t status)
 
 sht1x_error_t sht1x_status_read(uint8_t * status)
 {
-	sht1x_error_t result = SHT1X_ERROR_UNKNOWN;
+	sht1x_error_t result = SHT1X_ERROR_BUSY;
 
 	if (xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE) {
 		sht1x_device_prepare(SHT1X_SIDX_SREGR_FIN, sreg_read_pattern, sizeof(sreg_read_pattern));
@@ -718,9 +756,19 @@ sht1x_error_t sht1x_status_read(uint8_t * status)
 		TimerEnable(SHT1X_TIMER_BASE, SHT1X_TIMER);
 
 		if (xSemaphoreTake(interrupt_semaphore, portMAX_DELAY) == pdTRUE) {
-			result = device.error;
-			if (result == SHT1X_ERROR_OK && status != NULL) {
-				*status = device.data & 0xff;
+			if (device.error == SHT1X_ERROR_OK) {
+				uint8_t data[3] = {SREG_READ_SEQENCE, device.data & 0xff, sht1x_reverse_bits(device.crc)};
+				uint8_t crc = sht1x_crc(data, sizeof(data), device.crc_init);
+				if (crc != 0) {
+					device.status = device.data & 0xff;
+					if (status != NULL) {
+						*status = device.status;
+					}
+					device.crc_init = sht1x_reverse_bits(device.status);
+					result = device.error;
+				} else {
+					result = SHT1X_ERROR_INVALID_CRC;
+				}
 			}
 			xSemaphoreGive(lock);
 		}
@@ -729,7 +777,9 @@ sht1x_error_t sht1x_status_read(uint8_t * status)
 	return result;
 }
 
-
+/**
+ * TODO: replace bit-mask with simple enum and switch statement
+ */
 void sht1x_process()
 {
 	sht1x_debug_toggle();
@@ -847,19 +897,43 @@ void udma_error_isr_handler(void)
 }
 
 
+uint8_t sht1x_reverse_bits(uint8_t value)
+{
+	uint8_t i;
+	uint8_t reversed = value;
+	for (i = 0; i < 8; i++) {
+		reversed <<= 1;
+		reversed |= value & 0x01;
+		value >>= 1;
+	}
+	return reversed;
+}
+
+
+uint8_t sht1x_crc(uint8_t * data, size_t data_len, uint8_t initial_crc)
+{
+	uint8_t crc = initial_crc;
+	while (data_len) {
+		crc = crc8_lut[crc ^ *data];
+		data++;
+		data_len--;
+	}
+	return crc;
+}
+
+/**
+ * TODO: move to HW specific module
+ */
 void sht1x_init(void)
 {
 	lock = xSemaphoreCreateMutex();
 	interrupt_semaphore = xSemaphoreCreateBinary();
+
+	device.status = SHT1X_DEFAULT_STATUS;
+	device.crc_init = SHT1X_DEFAULT_CRC_INIT;
 
 	sht1x_gpio_init();
 
 	sht1x_timer_init();
 	sht1x_udma_init();
 }
-
-
-void sht1x_crc(uint8_t data, uint8_t crc)
-{
-}
-
