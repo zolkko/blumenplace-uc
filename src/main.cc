12df6cb3300b15589a703f276bb0a99480f22e19
zolkko <zolkko@gmail.com>
2014-06-19 22:53:51 +0400

    load address of the data segment has been fixed

--- src/main.cc
+++ src/main.cc
@@ -13,6 +13,10 @@ const uint32_t constant = 123;
 
 class demo_t {
 public:
+	static const uint32_t inc_def = 1;
+
+	static uint32_t test_value;
+
 	demo_t(const uint32_t d_) : d(d_) {
 	}
 
@@ -20,15 +24,18 @@ public:
 		d += demo_t::inc_def;
 	}
 private:
-	static const uint32_t inc_def = 1;
 	uint32_t d;
 };
+
+uint32_t demo_t::test_value = 12;
 
 int main(void)
 {
 	int a = 1;
 	int b = 2;
 	int c = a + b + constant;
+	int d = demo_t::inc_def;
+	int e = demo_t::test_value;
 
 	demo_t demo(c);
 	while (true) {
